a60fe76c2d9cfab1e3df2b3d5c2cee66a2b96e7f
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-05-06 13:01:49 +0530

    Day 6 : Majority Element --

---
+++ May/week1/6_MajorityElement.java
@@ -0,0 +1,37 @@
+/* 
+        Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+        You may assume that the array is non-empty and the majority element always exist in the array.
+
+        Example 1:
+
+        Input: [3,2,3]
+        Output: 3
+        Example 2:
+
+        Input: [2,2,1,1,1,2,2]
+        Output: 2
+*/
+
+class Solution {
+        public int majorityElement(int[] nums) {
+            if(nums.length == 0){
+                return 0;
+            }
+            int majEle = nums[0];
+            int count = 1;
+            for(int i = 1; i < nums.length; i++){
+                if(majEle == nums[i]){
+                    count++;
+                } else {
+                    count--;
+                }
+                
+                if(count == 0){
+                    majEle = nums[i];
+                    count = 1;
+                }
+            }
+            return majEle;
+        }
+    }
